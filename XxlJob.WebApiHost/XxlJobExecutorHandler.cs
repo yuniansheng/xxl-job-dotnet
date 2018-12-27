5d66d76ee8b146fe049718516641c38ead8ad0e2
yns余年胜 <nsyu@ctrip.com>
2018-12-27 15:24:47 +0800

    Hessian使用nuget包引入，XxlJobExecutorHandler添加日志

--- XxlJob.WebApiHost/XxlJobExecutorHandler.cs
+++ XxlJob.WebApiHost/XxlJobExecutorHandler.cs
@@ -9,27 +9,40 @@ using System.Threading;
 using System.Threading.Tasks;
 using XxlJob.Core.Executor;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace XxlJob.WebApiHost
 {
     internal class XxlJobExecutorHandler : HttpMessageHandler
     {
         private readonly JobExecutor _executor;
+        private readonly ILogger _logger;
 
         public XxlJobExecutorHandler(IServiceProvider services)
         {
             _executor = services.GetService<JobExecutor>();
+            _logger = services.GetService<ILoggerFactory>().CreateLogger<XxlJobExecutorHandler>();
         }
 
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            var inputStream = request.Content.ReadAsStreamAsync().Result;
-            byte[] responseBytes = _executor.HandleRequest(inputStream);
+            try
+            {
+                var inputStream = request.Content.ReadAsStreamAsync().Result;
+                byte[] responseBytes = _executor.HandleRequest(inputStream);
 
-            var response = request.CreateResponse(HttpStatusCode.OK);
-            response.Content = new ByteArrayContent(responseBytes);
-            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html") { CharSet = "UTF-8" };
-            return Task.FromResult(response);
+                var response = request.CreateResponse(HttpStatusCode.OK);
+                response.Content = new ByteArrayContent(responseBytes);
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html") { CharSet = "UTF-8" };
+                return Task.FromResult(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "XxlJobExecutorHandler handle request error.");
+                var response = request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+                return Task.FromResult(response);
+            }
+
         }
     }
 }
